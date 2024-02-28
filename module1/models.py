87e44ab260cf4051a4f669b0bf5b772fa2f1d662
lasyaupputuri <upputurilasya@gmail.com>
2024-02-28 16:24:28 +0530

    first commit

---
+++ module1/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Register(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(primary_key=True)
+    password = models.CharField(max_length=100)
+    phonenumber = models.IntegerField()
+
+    class Meta:
+        db_table='Register'
+
+class contactus(models.Model):
+    firstname = models.TextField(max_length=255)
+    lastname = models.TextField(max_length=255, default='')
+    email = models.EmailField(primary_key=True)
+    comments = models.TextField(max_length=255)
+
+    class Meta:
+        db_table = "contactus"
---
+++ review/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+
+def review_list(request):
+    if request.method == 'GET':
+        reviews = TouristReview.objects.all()
+        return render(request, 'reviewpage.html', {'reviews: reviews'})
+
+def add_review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST,request.FILES)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('review_list')
+    else:
+        form = ReviewForm()
+
+    return render(request, 'reviewpage.html',{'form' : form})
+
+from django import forms
+from .models import TouristReview
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = TouristReview
+        fields = ['text', 'rating', 'image']
+
+
